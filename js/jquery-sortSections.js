065703953ae432707f907f7a97d25c2c8bbe1c9b
bottomleya <bottomleya@hotmail.com>
2021-11-25 20:20:13 +1100

    Update jquery-sortSections.js

--- js/jquery-sortSections.js
+++ js/jquery-sortSections.js
@@ -29,69 +29,18 @@ jQuery.extend( jQuery.fn.dataTableExt.oSort, {
 		}
 
 		a = a.replace(/<[\s\S]*?>/g, "");
-		if (m.length == 4) {
-			// IPV4
-			for(i = 0; i < m.length; i++) {
-				item = m[i];
+		
+		for(i = 0; i < m.length; i++) {
+			item = m[i];
 
-				if(item.length == 1) {
-					x += "00" + item;
-				}
-				else if(item.length == 2) {
-					x += "0" + item;
-				}
-				else {
-					x += item;
-				}
+			if(item.length == 1) {
+				x += "00" + item;
 			}
-		}
-		else if (n.length > 0) {
-			// IPV6
-			var count = 0;
-			for(i = 0; i < n.length; i++) {
-				item = n[i];
-
-				if (i > 0) {
-					xa += ":";
-				}
-
-				if(item.length === 0) {
-					count += 0;
-				}
-				else if(item.length == 1) {
-					xa += "000" + item;
-					count += 4;
-				}
-				else if(item.length == 2) {
-					xa += "00" + item;
-					count += 4;
-				}
-				else if(item.length == 3) {
-					xa += "0" + item;
-					count += 4;
-				}
-				else {
-					xa += item;
-					count += 4;
-				}
+			else if(item.length == 2) {
+				x += "0" + item;
 			}
-
-			// Padding the ::
-			n = xa.split(":");
-			var paddDone = 0;
-
-			for (i = 0; i < n.length; i++) {
-				item = n[i];
-
-				if (item.length === 0 && paddDone === 0) {
-					for (var padding = 0 ; padding < (32-count) ; padding++) {
-						x += "0";
-						paddDone = 1;
-					}
-				}
-				else {
-					x += item;
-				}
+			else {
+				x += item;
 			}
 		}
 
