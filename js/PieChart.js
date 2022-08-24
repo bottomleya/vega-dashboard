9f14d8fba72a7004d6d1e681821679f1bd25be29
bottomleya <bottomleya@hotmail.com>
2022-08-24 16:10:14 +1000

    Update PieChart.js

--- js/PieChart.js
+++ js/PieChart.js
@@ -1,18 +1,18 @@
 class PieChart {
-  constructor(div, idStr) {
+  constructor(divGrid, idStr) {
     
-    this.parentDiv = div;
+    this.grid = divGrid;
     
     this.points = [];
     
     this.canvas_id = "canvas_" + String(idStr);
     
-    this.item_id = "idem_" + String(idStr);
+    this.item_id = "item_" + String(idStr);
     
     this.width="800";
     this.height="200";
     
-    
+    this.
        
   }
   
@@ -21,7 +21,8 @@ class PieChart {
   }
   
   html() {
-    return "<div class=\"item\"><div class=\"item-content resizable\" id=\"" + this.item_id + "\"><div class=\"x\"><i class=\"fa fa-xmark\"></i></div><div class=\"resizers inactive\"><div class=\"resizer bottom-right hidden\"></div><canvas id=\"" + this.canvas_id + "\" width=\"" + String(this.width) +"\" height=\"" + String(this.height) + "\"></canvas></div></div></div>"
+    this.htmlString = "<div class=\"item\"><div class=\"item-content resizable\" id=\"" + this.item_id + "\"><div class=\"x\"><i class=\"fa fa-xmark\"></i></div><div class=\"resizers inactive\"><div class=\"resizer bottom-right hidden\"></div><canvas id=\"" + this.canvas_id + "\" width=\"" + String(this.width) +"\" height=\"" + String(this.height) + "\"></canvas></div></div></div>";
+    return this.htmlString
   }
     
 }
