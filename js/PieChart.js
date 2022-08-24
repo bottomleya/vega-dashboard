a6789d26b883f6404a36adfec956c9aec10f973b
bottomleya <bottomleya@hotmail.com>
2022-08-24 16:25:47 +1000

    Update PieChart.js

--- js/PieChart.js
+++ js/PieChart.js
@@ -15,11 +15,15 @@ class PieChart {
   }
   
   setDOM() {
-    
+    var item = document.createElement('div');
+    item.setAttribute('class', 'item');
+    item.innerHTML = this.html();
+    document.body.appendChild(item);
+    grid.add(item);
   }
   
   html() {
-    this.htmlString = "<div class=\"item\"><div class=\"item-content resizable\" id=\"" + this.item_id + "\"><div class=\"x\"><i class=\"fa fa-xmark\"></i></div><div class=\"resizers inactive\"><div class=\"resizer bottom-right hidden\"></div><canvas id=\"" + this.canvas_id + "\" width=\"" + String(this.width) +"\" height=\"" + String(this.height) + "\"></canvas></div></div></div>";
+    this.htmlString = "<div class=\"item-content resizable\" id=\"" + this.item_id + "\"><div class=\"x\"><i class=\"fa fa-xmark\"></i></div><div class=\"resizers inactive\"><div class=\"resizer bottom-right hidden\"></div><canvas id=\"" + this.canvas_id + "\" width=\"" + String(this.width) +"\" height=\"" + String(this.height) + "\"></canvas></div></div>";
     return this.htmlString
   }
     
