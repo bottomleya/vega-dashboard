84f87529b2069c4831b77fc500894de01c18e6c7
bottomleya <bottomleya@hotmail.com>
2022-08-29 23:56:41 +1000

    Update Point.js

--- js/Point.js
+++ js/Point.js
@@ -1,1 +1,13 @@
 
+class Point {
+  constructor(name) {
+    this.name           =   name;
+    this.displayName    =   name;
+    this.units          =   "";
+    this.feature        =   false;
+    this.label          =   false;
+    this.categorical    =   false;
+    this.color          =   'rgb(54, 162, 235)';
+  }
+  
+}
