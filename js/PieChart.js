f3b0762c9bd8bc3217da4adf851d7c79f989a27f
bottomleya <bottomleya@hotmail.com>
2022-08-31 11:06:47 +1000

    Update PieChart.js

--- js/PieChart.js
+++ js/PieChart.js
@@ -7,48 +7,6 @@ class PieChart {
     this.item_id = "item_" + String(idStr);
     this.itemContent_id = "itemContent_" + String(idStr);
     
-    this.options = {
-      events: false,
-      animation: {
-        duration: 500,
-        easing: "easeOutQuart",
-        onComplete: function () {
-          var ctx = this.chart.ctx;
-          ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontFamily, 'normal', Chart.defaults.global.defaultFontFamily);
-          ctx.textAlign = 'center';
-          ctx.textBaseline = 'bottom';
-
-          this.data.datasets.forEach(function (dataset) {
-
-            for (var i = 0; i < dataset.data.length; i++) {
-              var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
-                  total = dataset._meta[Object.keys(dataset._meta)[0]].total,
-                  mid_radius = model.innerRadius + (model.outerRadius - model.innerRadius)/2,
-                  start_angle = model.startAngle,
-                  end_angle = model.endAngle,
-                  mid_angle = start_angle + (end_angle - start_angle)/2;
-
-              var x = mid_radius * Math.cos(mid_angle);
-              var y = mid_radius * Math.sin(mid_angle);
-
-              ctx.fillStyle = '#fff';
-              if (i == 3){ // Darker text color for lighter background
-                ctx.fillStyle = '#444';
-              }
-              var percent = String(Math.round(dataset.data[i]/total*100)) + "%";      
-              //Don't Display If Legend is hide or value is 0
-              if(dataset.data[i] != 0 && dataset._meta[0].data[i].hidden != true) {
-                ctx.fillText(dataset.data[i], model.x + x, model.y + y);
-                // Display percent in another line, line break doesn't work for fillText
-                ctx.fillText(percent, model.x + x, model.y + y + 15);
-              }
-            }
-          });               
-        }
-      }
-    };
-    
-    
     this.title = [];
     
     this.points = [];
@@ -119,7 +77,13 @@ class PieChart {
             hoverOffset: 4
           }]
         },
-        options: this.options
+        options: {
+          maintainAspectRatio: false,
+          labels: {
+            // render 'label', 'value', 'percentage', 'image' or custom function, default is 'percentage'
+            render: 'value'
+          }
+        }
     });
   }
   
